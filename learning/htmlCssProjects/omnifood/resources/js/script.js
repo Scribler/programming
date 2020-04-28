913837da5aafabe6b228cc6dd624f3fe6c502b53
Steven McIntyre <steven.mc2225@gmail.com>
2020-04-28 06:09:26 -0700

    added animations

--- learning/htmlCssProjects/omnifood/resources/js/script.js
+++ learning/htmlCssProjects/omnifood/resources/js/script.js
@@ -70,9 +70,34 @@ $(document).ready(function() {
        }
      });
 
+   //
+   // animations on scroll
+   //
 
+        $('.js--wp-1').waypoint(function(direction){
+           $('.js--wp-1').addClass('animated fadeIn'); // fade in 
+        }, {
+           offset:' 50%'
+        });
 
+        $('.js--wp-2').waypoint(function(direction){
+           $('.js--wp-2').addClass('animated fadeInUp'); // fade in up phone
 
+        }, {
+           offset:' 50%'
+        });
+
+        $('.js--wp-3').waypoint(function(direction){
+           $('.js--wp-3').addClass('animated fadeIn'); // fade in cities
+        }, {
+           offset:' 50%'
+        });
+   
+        $('.js--wp-4').waypoint(function(direction){
+           $('.js--wp-4').addClass('animated pulse'); // price plan pulse
+        }, {
+           offset:' 50%'
+        });
 
 });
 
