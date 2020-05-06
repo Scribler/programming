5327b08214d05721e958df5721b4af526931b841
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-06 15:46:13 -0700

    cleaned up 'programming' file

--- learning/JavascriptProjects/javascriptCourse/theBasics/logicalnot.js
+++
@@ -1,8 +0,0 @@
-// logical NOT (!)
-
-let user = false
-
-// The if statement will not run if the value is false.  This is why you use not, to turn the false user into a 'not'false user.
-if(!user){
-    console.log('User not logged in');
-}
--- projects/ruby/dungeonGame/dungeon_classes.rb
+++
@@ -1,7 +0,0 @@
-class Player
-  def backpack
-    @backpack = []
-  end
-  attr_accessor :name, :class
-  attr_accessor :attack, :speed
-end
