847945e2c889e1b4d6672f019029bbf005d6b23d
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-15 19:57:29 -0700

    working with strings in javascript

---
+++ learning/javascriptProjects/exampleProjects/js/strings.js
@@ -0,0 +1,42 @@
+// strings
+console.log('# examples of strings');
+console.log('hello, world')
+
+let email = "mario.bros@nintendo.jp";
+console.log(email);
+
+// string concatonation
+console.log('# concatonation of two strings');
+let firstname = 'Brandon';
+let lastname = 'Sanderson';
+let fullname = firstname + ' ' + lastname;
+
+console.log(firstname);
+console.log(lastname);
+console.log(fullname);
+
+// getting characters
+console.log('# logging out specific letters by character position');
+console.log(fullname[0]);
+console.log(fullname[5]);
+console.log(fullname[8]);
+
+// string length
+console.log('# number of characters in fullname:');
+console.log(fullname.length);
+
+
+
+// string methods
+// 1) A function is a snippet of code that performs a certain task.
+// 2) A method is a function that's associated with a particular object or data type(usually pre-built 
+// functions)
+console.log('# using the string  method "toUpperCase" & "toLowerCase" to capitalize all letters in a string');
+console.log(fullname.toUpperCase());
+console.log(fullname.toLowerCase());
+
+
+
+
+
+
