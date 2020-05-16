6ef5faa2b22e8437e123266097182d509e2d9756
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-16 15:54:00 -0700

    finished template strings section started arrays section

--- learning/javascriptProjects/exampleProjects/js/templateStrings.js
+++ learning/javascriptProjects/exampleProjects/js/templateStrings.js
@@ -6,13 +6,21 @@ const likes = 30;
 // ***EXAMPLES*** 
 
 
-
-
 // Concatenation Way
+let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+console.log(result);
 
 // Template String Way
+let result1 = `The blog called ${title} by ${author} has ${likes} likes.`;
+console.log(result1);
 
 // Creating HTML templates
+let html = `
+  <h2>#{title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes.</span>
+  `;
+console.log(html);
 
 
 
