2da9e88ed595e9fa6ed46a52f2f14c89ff767912
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-16 14:21:22 -0700

    finished numbers started template strings

--- learning/javascriptProjects/exampleProjects/js/numbers.js
+++ learning/javascriptProjects/exampleProjects/js/numbers.js
@@ -1,1 +1,63 @@
-console.log('boop');
+// variables
+let radius = 10;
+const pi = 3.14;
+let result = radius % 3;
+let result1 = pi * radius**2;
+let result2 = 5 * (10-3)**2;
+let likes = 10;
+// let result3 =
+// let result4 =
+// let result5 =
+// let result6 =
+
+
+// opperations
+  console.log(radius, pi);
+
+
+// math opperators +, -, *, /, **(to the power of), %(returns remainder)
+
+  console.log(10 / 2);
+
+  console.log(result);
+  console.log(result1);
+
+// order of operation - B I D M A S
+
+  console.log(result2);
+  // likes = likes + 1;
+
+// Shorthand notation
+  likes++; // does the same thing as (likes = likes + 1)
+  console.log(likes);
+  likes--; // '--' does the opposite of ++
+  console.log(likes);
+  likes += 10; // adds 10 to the current value
+  console.log(likes);
+  likes -= 10;
+  console.log(likes);
+  likes *= 2;
+  console.log(likes);
+  likes /= 2;
+  console.log(likes);
+
+// NaN (not a number)
+  
+  console.log(5 / 'hello');
+  console.log(5 * 'hello');
+
+// concatonate a variable into a string.
+
+  console.log('the blog has ' + likes + ' likes.')
+
+
+  // console.log(result3);
+  // console.log(result4);
+  // console.log(result5);
+  // console.log(result6);
+
+
+
+
+
+
---
+++ learning/javascriptProjects/exampleProjects/js/templateStrings.js
@@ -0,0 +1,18 @@
+// variables
+const title = 'Best Reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// ***EXAMPLES*** 
+
+
+
+
+// Concatenation Way
+
+// Template String Way
+
+// Creating HTML templates
+
+
+
