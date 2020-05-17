cf84cf390809a6885d375cc780d9c0d097669c38
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-17 09:46:39 -0700

    progress in arrays

--- learning/javascriptProjects/exampleProjects/js/arrays.js
+++ learning/javascriptProjects/exampleProjects/js/arrays.js
@@ -1,1 +1,28 @@
-console.log('foobar');
+// VARIABLES
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+let ages = [20, 25, 30, 35];
+let random = ['shaun', 'crystal', 30, 20];
+
+// EXPERIMENTING
+
+console.log(ninjas);
+console.log(ninjas[1]);
+ninjas[1] = 'ken' // updates position[1] in the array
+console.log(ninjas);
+
+
+console.log(ages);
+console.log(ages[2]);
+
+
+console.log(random);
+console.log(random[0]);
+
+
+console.log(ninjas.length);
+
+
+// ARRAY METHODS
+
+let result = ninjas.join(',');
+console.log(result);
