154dad332068ca81ea664d15b79b5bb653670f97
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-18 15:58:55 -0700

    finished type conversion and booleans

--- learning/javascriptProjects/exampleProjects/js/booleans.js
+++ learning/javascriptProjects/exampleProjects/js/booleans.js
@@ -28,5 +28,60 @@ console.log(result4);
 
 console.log(' ');
 // comparison operators
+let age = 25;
+
+console.log('Is age == 25?');
+console.log(age == 25);
+console.log('Is age == 30?');
+console.log(age == 30);
+console.log('Is age != 30?     **note** - "!" means "not"');
+console.log(age != 30);
+console.log('Is age != 25?');
+console.log(age != 25);
+console.log('is age greater than 20?');
+console.log(age > 20);
+console.log('is age less than or equal to 20?');
+console.log(age <= 25);
+
+console.log(' ');
+
+let name = 'shaun';
+
+console.log('is name equal to "shaun"');
+console.log(name == 'shaun');
+console.log('is name equal to "Shaun"');
+console.log(name == 'Shawn');
+console.log('is name shaun greater than name crystal?');
+console.log(name > 'crystal');
+console.log('is name shaun greater than name Shaun?');
+console.log(name > 'Shaun');
+console.log('is name Crystal greater than name shaun?');
+console.log(name > 'Crystal');
+console.log('lowercase letters are "greater" than all uppercase letters.')
+
+console.log(' ');
+// Comparison
+age1 = 25;
+
+// loose comparison
+console.log('loose comparison')
+console.log('age == 25 & age == "25"');
+console.log(age == 25);
+console.log(age == '25');
+console.log('age != 25 & age != "25"');
+console.log(age != 25);
+console.log(age != '25');
 
 
+console.log(' ');
+// strict comparison
+console.log('strict comparison');
+console.log('age === 25 & age === "25"');
+console.log(age === 25);
+console.log(age === '25');
+console.log('age !== 25 & age !== "25"');
+console.log(age !== 25);
+console.log(age !== '25');
+
+
+console.log(' ');
