c97784219d296ba326408936e37ae416c662fed4
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-30 11:12:22 -0700

    made a file for old blog desing so I can go back and forth to compare

---
+++ projects/blogOld/budgetApp/resources/js/sandbox.js
@@ -0,0 +1,15 @@
+const butt = document.querySelector('.bEntry')
+
+let fart = 20
+console.log('hello world');
+console.log('Hello World That is Capitalized!');
+console.log(`This number is stored in the variable called fart: ${fart}`);
+
+function blankEntry(){
+   var x = butt;
+   butt[0].innerhtml="hello World";
+};
+
+
+
+               <input id="incomeM" placeholder="Monthly Income">
