9a2ef70db30d3b524c678a4242af9ce9c078e9ef
Steven McIntyre <steven.mc2225@gmail.com>
2020-05-31 00:28:45 -0700

    fixed ajaxpractice so that it would display in local server. html file needed to be called index.html for it to work

--- projects/blog/resources/js/sandbox.js
+++ projects/blog/resources/js/sandbox.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+var person = {fname:'Joe', lname:'Jameson', age:44, occupation:'Millwright'};
+
+var text = "";
+var x;
+
+
+
+for(i = 0; i < 8; i++){
+  console.log(i);
+}
+
+
+for(x in person){
+  text += "<li>" + person[x] + "</li>" + "<br>";
+}
+
+document.getElementById('blogs').innerHTML = text;
+console.log(text);
