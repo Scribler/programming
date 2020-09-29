5ac847bf1ec8c3b00a97be318edd4c4d4e0d2409
Steven McIntyre <steven.mc2225@gmail.com>
2020-09-28 22:20:17 -0700

    program still isn't working.  Will do my own javascript to get it to work.  Something isn't right in the template file?

--- web-dev-course/2.CSS/996.interest-calculator/js/index.js
+++ web-dev-course/2.CSS/996.interest-calculator/js/index.js
@@ -1,48 +1,49 @@
 const slider = document.queryselector('#initial-amount');
-const sliderDisplay = display.queryselector('#initial-amount-display .amount')
-const { format } = new Intl.NumberFormat('en-US');
+const sliderDisplay = display.queryselector('#initial-amount-display .amount');
+const calculateButton = document.queryselector('.calculate-button');
+const interestRateInput = document.queryselector('#interest-rate');
 const yearInput = document.queryselector('#years');
-const interestRateInput = 
-const montlyRadio = 
+const monthlyRadio = document.queryselector('#monthly');
 
-
-
-
+const { format } = new Intl.NumberFormat('en-US');
 
 // initial startup
   sliderDisplay.innerHTML = format(slider.value);
 calculateFinalAmount();
-// const numberFormatter = new Intl.NumberFormat('en-US');
 
-
-
-sliderDsiplay.innerHTML = numberFormatter.format(slider.value);
+// Event Listeners
 slider.addEventListener('input', handleSlide);
+calculateButton.addEventListener('click', calculateFinalAmount);
 
 // event handler for when slider changes
 // @pram {event} event;
 
-
 function handleSlide(event) {
-  sliderDisplay.innerHTML = event.target.value;
-
+  sliderDisplay.innerHTML = format(event.target.value);
 }
 
+//
+// Calculates compound interest with formula A = P(1 + r/n)^tn where
+// A - final amount
+// P - principal amount
+// r - interest rate in decimal (e.g. 2% -> 0.02)
+// n - Times compounded per time period
+// t - Tiem period in years
+//
 
 function calculateFinalAmount() {
   const initialAmount = slider.value;
   const years = yearInput.value;
 
-// convert interest rate from percentage to decimal
-  const interest?Rate = interestRateInput.value / 100;
+  // convert interest rate from percentage to decimal
+  const interestRate = interestRateInput.value / 100;
 
   // compunding periods
   const compoundPerYear = monthlyRadio.checked ? 12 : 1;
 
-  const finalAmount = initialAmount = * Math.pow(1 + interestRate / compundPerYear, years * compoundPerYear);
+  const finalAmount = initialAmount * Math.pow(1 + interestRate / compundPerYear, years * compoundPerYear);
 
   document.querySelector('.result .amount').innerHTML = format(Math.floor(finalAmount));
-  document.querySelector('.num-years')
-  document.querySelector('.plural')
-
+  document.querySelector('.num-years').innerHTML = years
+  document.querySelector('.plural').style.display = years > 1 ? 'inline' : 'none'
 }
