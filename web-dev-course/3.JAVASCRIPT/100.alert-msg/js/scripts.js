a2c30867b9a927e7b23f6f90ef1ca9aa864f2c07
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-05 22:18:39 -0700

    finished editing html javascript lesson

--- web-dev-course/3.JAVASCRIPT/100.alert-msg/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/100.alert-msg/js/scripts.js
@@ -9,6 +9,14 @@ document.getElementById("button2").onclick=function(){
   alert("You have just clicked me!");
 }
 
+document.getElementById("button1").onclick=function(){
+  document.getElementById("facebook").innerHTML="<p>Facebook</p>";
+}
+
+document.getElementById("button3").ondblclick=function(){
+  document.getElementById("apple").innerHTML="<h3>Hear is a list of cars</h3><ul><li>car 1</li><li>Car 2</li><li>CAR 3</li></ul>";
+}
+
 // these both will work, but the first one is propper. Get a program to check your code in vim
 // window.alert("welcome to javascript");
 // alert("welcome to javascript");
