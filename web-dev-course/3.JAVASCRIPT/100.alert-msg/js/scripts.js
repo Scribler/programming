6400c04107572ff4f69d7361caf33935fc8a5b17
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-05 22:01:10 -0700

    completed first two lessons relating to javascript.

--- web-dev-course/3.JAVASCRIPT/100.alert-msg/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/100.alert-msg/js/scripts.js
@@ -1,3 +1,15 @@
 let hello = "hello world";
 
+
 console.log(hello);
+console.log('this is the first javascript lesson')
+
+// There are many other things you can put other than '.onclick' relating to hover double click etc. etc.
+document.getElementById("button2").onclick=function(){
+  alert("You have just clicked me!");
+}
+
+// these both will work, but the first one is propper. Get a program to check your code in vim
+// window.alert("welcome to javascript");
+// alert("welcome to javascript");
+
