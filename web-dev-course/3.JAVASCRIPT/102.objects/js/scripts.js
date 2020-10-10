e1ef362663b0c7d10bc2bbc04bbaf94ca43e5053
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-10 12:10:21 -0700

    created notes page and fixed broken template files (file paths were incorrect)

--- web-dev-course/3.JAVASCRIPT/102.objects/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/102.objects/js/scripts.js
@@ -34,7 +34,31 @@ myOtherPhone.model = "Iphone 5";
 window.console.log(myPhone);
 window.console.log(myOtherPhone);
 
+window.console.log(window);
+window.console.log(console);
 
+function nope(){
+  // If using "console" as a variable?
+  // NO LONGER - use 'console.log();'
+  // INSTEAD - use 'window.console.log();'
+  var console = 42;
+  try {
+    // this won't work because 'console' has been shadowed by the previous variable
+    console.log("you won't see this");
+  } catch (e) {
+  }
+  try {
+    window.console.log("you will see this");
+  } catch (e) {
+  }
+}
+nope();
+
+//wiindow is the global variable in this case.  Although, it isn't always.  Aparently in NODEJS 'global' is the global variable, so don't bother to keep using window.console.log() that could bite you later on.
+
+
+// console.log(console);
+console.log('As long as console was only used as a variable in a function and not outside of the function, console.log will still work.  So there is no reason to use window.console.log() unless you are using it within a function that has console as a variable.  You do not need to worry about if it is used as a variable outside of a function because then it would disable "window.console.log();" as well as "console.log()" so it is irrelevant.');
 
 
 
