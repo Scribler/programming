35bcd3c99ca62fb7042bc44fd70f973d712a2ed5
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-13 22:15:56 -0700

    started to use if statements

--- web-dev-course/3.JAVASCRIPT/103.if-and-switch/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/103.if-and-switch/js/scripts.js
@@ -2,3 +2,26 @@
 var hello = 'Hello world';
 
 window.console.log(hello);
+
+
+
+var speed = 80;
+
+if (speed > 70){
+  alert("You're going to fast!");
+} else {
+  alert("You're speed is fine");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
