7dd8308181f18eba53636f786df543fcfc9f97f4
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-21 17:36:35 -0700

    update

--- web-dev-course/3.JAVASCRIPT/103.if-and-switch/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/103.if-and-switch/js/scripts.js
@@ -68,21 +68,25 @@ let i;
 
 // prints '0 - 4'
 for(i = 0; i < 5; i++){
-  document.getElementById("myParagraph").innerHTML += i + "<br />";
+  // document.getElementById("myParagraph").innerHTML += i + "<br />";
+  document.getElementById("myParagraph").innerHTML += i + " ";
 }
 
 // prints 7'squared' to 0 (decreasing)
 for(i=7; i>0; i--){
-  document.getElementById("myParagraph").innerHTML += "<br />" + Math.pow(i,2);
+  // document.getElementById("myParagraph").innerHTML += "<br />" + Math.pow(i,2);
+  document.getElementById("myParagraph").innerHTML += " " + Math.pow(i,2);
 }
 
 var shoppingList = ["bread", "milk", "eggs"];
 
 for(let i=0; i < shoppingList.length; i++){
   if(i === 0){
-    document.getElementById("myParagraph").innerHTML += "<br />" + shoppingList[i] + "<br />";
+    // document.getElementById("myParagraph").innerHTML += "<br />" + shoppingList[i] + "<br />";
+    document.getElementById("myParagraph").innerHTML += " " + shoppingList[i] + " ";
   } else {
-  document.getElementById("myParagraph").innerHTML += shoppingList[i] + "<br />";
+  // document.getElementById("myParagraph").innerHTML += shoppingList[i] + "<br />";
+  document.getElementById("myParagraph").innerHTML += shoppingList[i] + " ";
   }
 }
 
@@ -94,14 +98,16 @@ for(i = 0; i < numList.length; i++){
   console.log(numList);
 }
 
-document.getElementById("myParagraph").innerHTML += "<br />" + numList 
+// document.getElementById("myParagraph").innerHTML += "<br />" + numList
+document.getElementById("myParagraph").innerHTML += " " + numList;
 
 // While Loops
 
 var j = 0;
 
 while(j<5){
-  document.getElementById("while").innerHTML += "<br />" + j;
+  // document.getElementById("while").innerHTML += "<br />" + j;
+  document.getElementById("while").innerHTML += " " + j;
   j++;
 }
 
@@ -114,13 +120,71 @@ function initiateGame(){
     if(itemPrice <= balance){
     itemsBought++
     balance -= itemPrice;
-      document.getElementById("balance").innerHTML += "<p>Item Price: $" + itemPrice + ", Current Balance: $" + balance + "</p>";
+      document.getElementById("balance").innerHTML += " Item Price: $" + itemPrice + ", Current Balance: $" + balance + " , #";
     }
   }
   document.getElementById("balance").innerHTML += "<p>Items Bought: " + itemsBought + "</p>";
 }
 
-// Regular Expressions
+// Regular Expressions (regex)
+// use it to validate a user input
+
+//
+// SEARCH
+//
+var str = "Atlanta";
+// 'i' makes the pattern search case insensitive
+// 'g' makes the pattern search case globally 
+var pattern = /a/i;
+var patterng = /a/g;
+var patternl = /l/i;
+// the case insensitive search below returns '0' - it means it found an 'a' at position '0' iin the variable
+document.getElementById('regexSearch').innerHTML += "<br />" + "<h3>" + "Searches within the word 'Atlanta'." + "</h3>"
+console.log(str.search(pattern));
+document.getElementById('regexSearch').innerHTML += "Case insensitive search for 'a' returns: " + str.search(pattern) + "<br />";
+// This global search should return '3', as it is case sensitive and returns a result for the position of the first LOWER CASE a.
+console.log(str.search(patterng));
+document.getElementById('regexSearch').innerHTML += "Global (case sensitive) search for 'a' returns: " + str.search(patterng) + "<br />";
+// this search for 'l' should return '2'
+console.log(str.search(patternl));
+document.getElementById('regexSearch').innerHTML += "Case insensitive search for 'l' returns: " + str.search(patternl) + "<br />";
+
+//
+// MATCH
+//
+
+var matstr = "Atlanta"
+var pattern2 = /a/i;
+var pattern2g = /a/g;
+var pattern2gi = /a/gi;
+
+document.getElementById('regexmatch').innerHTML += "Case insensitive 'match' for 'a' in word 'Atlanta': " + matstr.match(pattern2) + "<br />";
+document.getElementById('regexmatch').innerHTML += "<sub>This returns 'a,a' because there are 2 lower case a's in 'Atlanta'.</sub>" + "<br />";
+document.getElementById('regexmatch').innerHTML += "Global 'match' for 'a' in word 'Atlanta': " + matstr.match(pattern2g) + "<br />";
+document.getElementById('regexmatch').innerHTML += "<sub>This returns 'A,a,a' because the global search is now also case insensitive.</sub>" + "<br />";
+document.getElementById('regexmatch').innerHTML += "Global, Case insensitive 'match' for 'a' in word 'Atlanta': " + matstr.match(pattern2gi);
+
+//
+// TEST
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
