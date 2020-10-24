405d5b57826150d717c2ae0525e2f3217c5ab765
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-23 19:40:08 -0700

    completed basic form. not sure of function yet

--- projects/budgetApp/js/scripts.js
+++ projects/budgetApp/js/scripts.js
@@ -1,2 +1,16 @@
 
 console.log("hello world");
+
+// buttons and interaction with database.
+
+
+var incomebw = 1450.00;
+var income = incomebw * 2;
+// var remainingSpending = income - 'expenses'
+
+document.getElementById("incomeTotal").value = income;
+document.getElementById("incomeBiWeekly").value = incomebw;
+
+
+// How to make expenses come off of income?
+//
