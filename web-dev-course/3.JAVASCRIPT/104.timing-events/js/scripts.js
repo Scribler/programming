799f8bd61ab8aa7619d20add1f7e04d165925fb7
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-30 06:58:48 -0700

    got first counter working. Need to mess around with it more

--- web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
@@ -1,3 +1,9 @@
 console.log("hello World!")
 
-document.getElementById("content").innerHTML += "<br />" + "Hello World";
+var delayedMessage = document.getElementById("three-second-message").innerHTML += "Three seconds have passed!";
+
+var counter = document.getElementById("counter");
+var x = 0;
+var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
+
+
