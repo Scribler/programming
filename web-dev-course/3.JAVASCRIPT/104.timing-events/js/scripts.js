4c88538addfb1964c6b85e3cdcaca47e9c4ed07f
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-31 10:18:41 -0700

    update, timer stop still not figured out.  Time to keep moving with lessons

--- web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
@@ -1,16 +1,33 @@
 
 var counter = document.getElementById("counter");
-// var counter = document.getElementById("counter").innerHTML;
-
 var x = 0;
-// var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
-// var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
+var myCounter = setInterval(function(){x++; counter.innerHTML = x}, 1000);
 
-  var startCount = setInterval(function(){x++; counter.innerHTML = x},1000);
-function startCounter(){
-  var startCount = setInterval(function(){x++; counter.innerHTML = x},1000);
+function restartCounter() {
+  var myCounter = setInterval(function(){x++; counter.innerHTML = x}, 1000);
+
+  function restopCounter(x) {
+    if(document.getElementById("stop").click == true){
+      clearInterval(myCounter);
+    }
+  }
 }
 
-function stopCountTwo(){
-  clearInterval(counter);
+
+
+// timeout
+
+
+var myTimer;
+
+function clock() {
+  myTimer = setInterval(myClock, 1000);
+  var c = 0;
+  function myClock() {
+    document.getElementById("demo").innerHTML = ++c;
+    if (c == 50) {
+      clearInterval(myTimer);
+      alert("reached 50");
+    }
+  }
 }
