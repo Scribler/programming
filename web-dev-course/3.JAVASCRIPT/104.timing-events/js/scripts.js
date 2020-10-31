84e8365ee2ba98d50bfee918a42a24fb36c4c51f
Steven McIntyre <steven.mc2225@gmail.com>
2020-10-30 23:19:53 -0700

    update timer still not restarting

--- web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
@@ -1,9 +1,16 @@
-console.log("hello World!")
-
-var delayedMessage = document.getElementById("three-second-message").innerHTML += "Three seconds have passed!";
 
 var counter = document.getElementById("counter");
+// var counter = document.getElementById("counter").innerHTML;
+
 var x = 0;
-var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
+// var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
+// var myCounter = setInterval(function(){x++; counter.innerHTML = x},1000);
 
+  var startCount = setInterval(function(){x++; counter.innerHTML = x},1000);
+function startCounter(){
+  var startCount = setInterval(function(){x++; counter.innerHTML = x},1000);
+}
 
+function stopCountTwo(){
+  clearInterval(counter);
+}
