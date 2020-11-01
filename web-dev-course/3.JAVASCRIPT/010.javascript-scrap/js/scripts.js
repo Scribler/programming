e2b686d3df0d787860ca723c7f1db0d5cbe3643f
Steven McIntyre <steven.mc2225@gmail.com>
2020-11-01 12:28:39 -0800

    added a new file for javascript testing. Got the color changing script to work!

---
+++ web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
@@ -0,0 +1,12 @@
+backgroundColors = ['green', 'orange', 'red', 'purple'];
+var x = 0;
+
+var background = setInterval(function(){
+  x++;
+  document.getElementById('articleColor').style.backgroundColor = backgroundColors[x];
+  if(x >= 3){
+    x = 0;
+  }
+},1000);
+  
+
--- web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/104.timing-events/js/scripts.js
@@ -1,16 +1,31 @@
-//Course version of counter
+// COURSE VERSION OF COUNTER
 var counter = document.getElementById("counter");
 var x = 0;
 
   // simply setting the variable makes the counter start on page load
 var myCounter = setInterval(function(){x++; counter.innerHTML = x}, 1000);
+var counterStop = setTimeout(function(){
+  clearInterval(myCounter);
+  document.getElementById("counterFinished").innerHTML = "Stoped Counter. It has exeded 50 seconds";
+}, 51000);
+
+// CHANGING BACKGROUND COLORS
+var colors = ["\"$blue1\"", "\"$green1\"", "\"$yellow1\"", "\"$red1\""];
+console.log(colors[1])
+var x = 0;
+var backgroundColors = setInterval(function(x){
+  // document.getElementById("headerHeading").style.color = colors[x];
+  x++;
+  console.log(colors[x]);
+  if(x > 3){
+    x = 0;
+  }
+}, 1000);
 
 
 
 
-
-
-// My own version of a timer
+// MY OWN VERSION OF A TIMER
 var myTimer;
 
 function clock() {
@@ -26,7 +41,7 @@ function clock() {
 }
 
 
-// timeout
+// TIMEOUT
 
 var delayedWelcomeMessage = setTimeout(function(){
   document.getElementById("delay").innerHTML = "Welcome to our page!";
@@ -36,3 +51,30 @@ var delayedWelcomeMessage = setTimeout(function(){
 function confirmation(){
   console.log("button clicked, if before 3 seconds, then the timed message was prevented!");
 }
+
+
+// WINDOW AND SCREEN OBJECTS
+
+// need multiple calls for the different browsers
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var windowWidth= window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var screenHeight = window.screen.height; // This gets the hardware screen height, NOT the browser screen width
+var screenWidth = window.screen.width; // This gets the hardware screen width, NOT the browser screen width
+
+// logging window height and width
+console.log(windowHeight);
+document.getElementById("windowHeight").innerHTML = windowHeight;
+console.log(windowWidth);
+document.getElementById("windowWidth").innerHTML = windowWidth;
+
+// logging screen height and width
+
+console.log(screenHeight);
+document.getElementById("screenHeight").innerHTML = screenHeight;
+console.log(screenWidth);
+document.getElementById("screenWidth").innerHTML = screenWidth;
+
+
+
+
+
