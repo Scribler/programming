f920556670e402940561f62faed9f651f2025776
Steven McIntyre <steven.mc2225@gmail.com>
2020-11-15 17:38:22 -0800

    fixed laggy button

--- web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
@@ -1,6 +1,16 @@
 console.log('hello World');
 
-
+function btnswap(){
+  if(document.getElementById("startReset").innerHTML == "Reset"){
+    document.getElementById("startReset").innerHTML = "Start";
+    document.getElementById("startReset").classList.remove("reset");
+    document.getElementById("startReset").classList.add("start");
+  } else {
+    document.getElementById("startReset").innerHTML = "Reset";
+    document.getElementById("startReset").classList.remove("start");
+    document.getElementById("startReset").classList.add("reset");
+  }
+}
 
 
 
