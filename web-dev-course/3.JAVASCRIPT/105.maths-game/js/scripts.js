889abc3cc10942b7291d4f4be61b04559a1f7968
Steven McIntyre <steven.mc2225@gmail.com>
2020-11-21 13:21:19 -0800

    update

--- web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
@@ -1,5 +1,4 @@
-console.log('hello World');
-
+// Change start/reset button from 'start' to 'reset' and back
 function btnswap(){
   if(document.getElementById("startReset").innerHTML == "Reset"){
     document.getElementById("startReset").innerHTML = "Start";
@@ -12,8 +11,29 @@ function btnswap(){
   }
 }
 
+// if we click on the start/reset?
+    // if we are playing?
+        // reload page
 
+    // if we are not playing?
+        // set score to 0
+        // show countdownbox
+        // reduce time by 1s in loops
+        // time left?
+            // yes -> continue
+            // no -> gameover
+        // change button to reset
+        // generate new Q&A
 
+// if we click on answer box
+    // if we are playing?
+        // correct answer
+            // yes?
+                // Increase score by 1
+                // show correct box for 1s
+                // generate new Q&A
+            // no?
+                // show try again for 1s
 
 
 
@@ -90,15 +110,4 @@ function btnswap(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Javascript is loading to the end!")
