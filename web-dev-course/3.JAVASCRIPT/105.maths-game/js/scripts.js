5b7f5c24667640cf8a756f4d419de5267680881b
Steven McIntyre <steven.mc2225@gmail.com>
2020-11-22 22:30:56 -0800

    game displays question and answers on start. Next stage is to work on answer selection

--- web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/105.maths-game/js/scripts.js
@@ -1,15 +1,97 @@
-// Change start/reset button from 'start' to 'reset' and back
+var score = 200;
+
+
+// Change start/reset button functions
+// - swap button name
+// - make timer visible
+// - generate first question
+// - reset page
 function btnswap(){
+  //reload page on click
   if(document.getElementById("startReset").innerHTML == "Reset"){
-    document.getElementById("startReset").innerHTML = "Start";
-    document.getElementById("startReset").classList.remove("reset");
-    document.getElementById("startReset").classList.add("start");
+    location.reload();
+  // start game on click
   } else {
+    // change button text to reset
     document.getElementById("startReset").innerHTML = "Reset";
     document.getElementById("startReset").classList.remove("start");
     document.getElementById("startReset").classList.add("reset");
+    // make timer visible
+    document.getElementById("time").classList.add("visible");
+    // generate first question
+    genquestion();
   }
 }
+
+
+
+// Show Countdown Timber box and display gameover box when time is up
+function timerstart(){
+  var counter = document.getElementById("remaining-time-value");
+  var x = 60;
+  var myCounter = setInterval(function(){
+    x--;
+    counter.innerHTML = x;
+    if (x == 0){
+      clearInterval(myCounter);
+      document.getElementById("score").innerHTML = score + "!";
+      document.getElementById("finished").classList.add("gamevisible");
+    }
+  }, 1000);
+}
+
+// Generate new Questions
+
+var numgenerator = function(x){
+  return Math.floor(Math.random() * x);
+}
+
+
+function genquestion(){
+  //generate question and answers
+  var x = numgenerator(11);
+  var y = numgenerator(11);
+  var question = x + "x" + y;
+  var answer = x * y;
+  // find a box to put the answer
+  var answerboxnum = numgenerator(4);
+  var answerbox = "a" + answerboxnum;
+  console.log(answerbox);
+  // display question
+  document.getElementById("question").innerHTML = question;
+  // display answer in a random answer box
+  document.getElementById(answerbox).innerHTML = answer;
+
+  //generate wrong answers
+  // - generate wrong answer boxes
+  var boxes = [0, 1, 2, 3];
+
+  function wrongbox(x){
+    boxes.splice(answerboxnum, 1);
+    console.log(boxes);
+  }
+  wrongbox(answerboxnum);
+
+  // - generate wrong answers and insert into boxes
+  for(x = 0; x < boxes.length; x++){
+    var a = numgenerator(11);
+    var b = numgenerator(11);
+    var boxnum = "a" + boxes[x];
+    console.log(boxnum);
+    var wronganswer = a*b;
+    document.getElementById(boxnum).innerHTML = wronganswer;
+  }
+  // Wrong question Click
+  // Correct question Click
+  function questioncheck(){
+  
+  }
+}
+
+
+
+
+
 
 // if we click on the start/reset?
     // if we are playing?
@@ -34,7 +116,6 @@ function btnswap(){
                 // generate new Q&A
             // no?
                 // show try again for 1s
-
 
 
 
