d816cc1da9bc28bfbb66d4d95135e53e37c0386b
Steven McIntyre <steven.mc2225@gmail.com>
2020-12-02 21:04:08 -0800

    finished the technical bits of my counter application. Still needs finishing touches and notes

--- web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
@@ -1,12 +1,123 @@
-// backgroundColors = ['green', 'orange', 'red', 'purple'];
-// var x = 0;
-//
-// var background = setInterval(function(){
-  // x++;
-  // document.getElementById('articleColor').style.backgroundColor = backgroundColors[x];
-  // if(x >= 3){
-    // x = 0;
-  // }
-// },1000);
-  
+// VARIABLES
+
+var counting = false;
+var counter_feed = document.getElementById("number");
+var counter_number = 0;
+var counter;
+
+// COUNTER FUNCTIONS
+
+function forwardcount(){
+  counter = setInterval(function(){
+    counter_number++;
+    counter_feed.innerHTML = counter_number;
+    counting = true;
+  }, 1000);
+}
+
+
+
+// FORWARD COUNTER
+
+document.getElementById("forwards").onclick = function(){
+  if(counting == true){
+    console.log("Counter Already Running");
+  } else {
+    // forwardcount();
+    counter = setInterval(function(){
+      counter_number++;
+      counter_feed.innerHTML = counter_number;
+      counting = true;
+    }, 1000);
+  }
+}
+
+// BACKWARDS COUNTER
+
+
+document.getElementById("backwards").onclick = function(){
+  if(counting == true){
+    console.log("Counter Already Running");
+  } else {
+    // forwardcount();
+    counter = setInterval(function(){
+      counter_number--;
+      counter_feed.innerHTML = counter_number;
+      counting = true;
+    }, 1000);
+  }
+}
+
+// TENS COUNTER
+
+document.getElementById("tens").onclick = function(){
+  if(counting == true){
+    console.log("Counter Already Running");
+  } else {
+    // forwardcount();
+    counter = setInterval(function(){
+      counter_number = counter_number + 10;
+      counter_feed.innerHTML = counter_number;
+      counting = true;
+    }, 1000);
+  }
+}
+
+// SEVENS COUNTER
+
+document.getElementById("sevens").onclick = function(){
+  if(counting == true){
+    console.log("Counter Already Running");
+  } else {
+    // forwardcount();
+    counter = setInterval(function(){
+      counter_number = counter_number + 7;
+      counter_feed.innerHTML = counter_number;
+      counting = true;
+    }, 1000);
+  }
+}
+
+
+
+
+
+
+// CUSTOM COUNTER?
+
+
+
+// ADD A COUNTER BUTTON WITH A PLACE TO ENTER A VALUE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// STOP COUNTER
+
+document.getElementById("stop").onclick = function(){
+  clearInterval(counter);
+  counting = false;
+}
+
+// CLEAR COUNTER
+
+document.getElementById("clear").onclick = function(){
+  counter_number = 0;
+  counter_feed.innerHTML = counter_number;
+}
+
+
+
+
 
