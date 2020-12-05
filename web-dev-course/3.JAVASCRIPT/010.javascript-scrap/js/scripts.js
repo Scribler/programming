5c9cacf63c8ecd58eadcfc87a6845db171c095b4
Steven McIntyre <steven.mc2225@gmail.com>
2020-12-05 14:38:59 -0800

    figured out a better way to code the timer and roughly implemented it alongside the old one.  Needs to be integrated

--- web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
@@ -1,12 +1,13 @@
 // VARIABLES
 
-var counting = false;
-var counter_feed = document.getElementById("number");
-var counter_number = 0;
-var counter;
+var counting = false; // Program status
+var counter_feed = document.getElementById("number"); // gets counter number 'div'
+var counter_number = 0; // sets counter number to zero
+var counter; // blank variable to set interval to (and enter into 'clearInterval afterwords')
 
 // COUNTER FUNCTIONS
 
+// count by ones
 function forwardcount(){
   counter = setInterval(function(){
     counter_number++;
@@ -15,6 +16,13 @@ function forwardcount(){
   }, 1000);
 }
 
+function count(x){
+  counter = setInterval(function(){
+    counter_number = x;
+    counter_feed.innerHTML = counter_number;
+    counting = true;
+  }, 1000);
+}
 
 
 // FORWARD COUNTER
@@ -79,30 +87,6 @@ document.getElementById("sevens").onclick = function(){
 }
 
 
-
-
-
-
-// CUSTOM COUNTER?
-
-
-
-// ADD A COUNTER BUTTON WITH A PLACE TO ENTER A VALUE
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // STOP COUNTER
 
 document.getElementById("stop").onclick = function(){
@@ -116,6 +100,98 @@ document.getElementById("clear").onclick = function(){
   counter_number = 0;
   counter_feed.innerHTML = counter_number;
 }
+
+
+
+
+
+
+
+// CUSTOM COUNTER?
+
+
+
+// ADD A COUNTER BUTTON WITH A PLACE TO ENTER A VALUE
+
+
+var counter_num = 0;
+var number_value = document.getElementById("value");
+var actuator;
+var coount;
+
+
+// ACTIVATE ADD BUTTON
+document.getElementById("add").onclick = function(){
+	count("add", number_value);
+}
+
+// ACTIVATE SUBTRACT BUTTON
+document.getElementById("subtract").onclick = function(){
+	count("sub", number_value);
+}
+
+// ACTIVATE MULTIPLY BUTTON
+document.getElementById("multiply").onclick = function(){
+	count("mult", number_value);
+}
+
+// ACTIVATE DIVIDE BUTTON
+document.getElementById("divide").onclick = function(){
+	count("div", number_value);
+}
+
+
+
+
+
+
+
+function count(operation){
+  coount = setInterval(function(){
+    
+    //add
+    if(operation == "add"){
+      actuator = number_value.value
+      document.getElementById("result").innerHTML = counter_num;
+			counter_num += parseInt(actuator);
+    }
+    //subtract
+    if(operation == "sub"){
+      actuator = number_value.value
+      document.getElementById("result").innerHTML = counter_num;
+			counter_num -= parseInt(actuator);
+    }
+
+    //multiply
+     if(operation == "mult"){
+      actuator = number_value.value
+      document.getElementById("result").innerHTML = counter_num;
+			counter_num *= parseInt(actuator);
+    }
+
+    //divide
+     if(operation == "div"){
+      actuator = number_value.value
+      document.getElementById("result").innerHTML = counter_num;
+			counter_num /= parseInt(actuator);
+    }
+  }, 1000);
+}
+
+document.getElementById("stopping").onclick = function(){
+  clearInterval(coount);
+}
+
+// HTML
+
+
+
+
+
+
+
+
+
 
 
 
