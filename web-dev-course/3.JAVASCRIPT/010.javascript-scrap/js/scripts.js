ce666c7d56e143c522d62d215de81d8f274f636b
Steven McIntyre <steven.mc2225@gmail.com>
2020-12-05 23:03:40 -0800

    fixed counter sync and overlapping counters problems

--- web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
+++ web-dev-course/3.JAVASCRIPT/010.javascript-scrap/js/scripts.js
@@ -1,93 +1,76 @@
+
 // VARIABLES
+var counting = false;
+var counter_num = 0;
+var number_value = document.getElementById("value");
+var actuator;
+var counter;
 
-var counting = false; // Program status
-var counter_feed = document.getElementById("number"); // gets counter number 'div'
-var counter_number = 0; // sets counter number to zero
-var counter; // blank variable to set interval to (and enter into 'clearInterval afterwords')
+// FUNCTIONS
 
-// COUNTER FUNCTIONS
-
-// count by ones
-function forwardcount(){
+function count(operation){
+  counting = true;
   counter = setInterval(function(){
-    counter_number++;
-    counter_feed.innerHTML = counter_number;
-    counting = true;
+  actuator = number_value.value
+  //add
+  if(operation == "add"){
+    counter_num += parseInt(actuator);
+    document.getElementById("number").innerHTML = counter_num;
+  } 
+  //subtract
+  if(operation == "sub"){
+    counter_num -= parseInt(actuator);
+    document.getElementById("number").innerHTML = counter_num;
+  }
+
+  //multiply
+   if(operation == "mult"){
+      counter_num *= parseInt(actuator);
+      document.getElementById("number").innerHTML = counter_num;
+    }
+
+  //divide
+   if(operation == "div"){
+      counter_num /= parseInt(actuator);
+      document.getElementById("number").innerHTML = counter_num;
+    }
   }, 1000);
 }
 
-function count(x){
-  counter = setInterval(function(){
-    counter_number = x;
-    counter_feed.innerHTML = counter_number;
-    counting = true;
-  }, 1000);
+function stop(){
+  clearInterval(counter);
+  counting = false;
 }
 
+// BUTTONS
 
-// FORWARD COUNTER
-
-document.getElementById("forwards").onclick = function(){
-  if(counting == true){
-    console.log("Counter Already Running");
-  } else {
-    // forwardcount();
-    counter = setInterval(function(){
-      counter_number++;
-      counter_feed.innerHTML = counter_number;
-      counting = true;
-    }, 1000);
-  }
+// activate add button
+document.getElementById("add").onclick = function(){
+  stop();
+	count("add", number_value);
 }
 
-// BACKWARDS COUNTER
-
-
-document.getElementById("backwards").onclick = function(){
-  if(counting == true){
-    console.log("Counter Already Running");
-  } else {
-    // forwardcount();
-    counter = setInterval(function(){
-      counter_number--;
-      counter_feed.innerHTML = counter_number;
-      counting = true;
-    }, 1000);
-  }
+// activate subtract button
+document.getElementById("subtract").onclick = function(){
+  stop();
+	count("sub", number_value);
 }
 
-// TENS COUNTER
-
-document.getElementById("tens").onclick = function(){
-  if(counting == true){
-    console.log("Counter Already Running");
-  } else {
-    // forwardcount();
-    counter = setInterval(function(){
-      counter_number = counter_number + 10;
-      counter_feed.innerHTML = counter_number;
-      counting = true;
-    }, 1000);
-  }
+// activate multiply button
+document.getElementById("multiply").onclick = function(){
+  stop();
+	count("mult", number_value);
 }
 
-// SEVENS COUNTER
-
-document.getElementById("sevens").onclick = function(){
-  if(counting == true){
-    console.log("Counter Already Running");
-  } else {
-    // forwardcount();
-    counter = setInterval(function(){
-      counter_number = counter_number + 7;
-      counter_feed.innerHTML = counter_number;
-      counting = true;
-    }, 1000);
-  }
+// activate divide button
+document.getElementById("divide").onclick = function(){
+  stop();
+	count("div", number_value);
 }
-
 
 // STOP COUNTER
+
+
 
 document.getElementById("stop").onclick = function(){
   clearInterval(counter);
@@ -97,94 +80,9 @@ document.getElementById("stop").onclick = function(){
 // CLEAR COUNTER
 
 document.getElementById("clear").onclick = function(){
-  counter_number = 0;
-  counter_feed.innerHTML = counter_number;
+  counter_num= 0;
+  number.innerHTML = counter_num;
 }
-
-
-
-
-
-
-
-// CUSTOM COUNTER?
-
-
-
-// ADD A COUNTER BUTTON WITH A PLACE TO ENTER A VALUE
-
-
-var counter_num = 0;
-var number_value = document.getElementById("value");
-var actuator;
-var coount;
-
-
-// ACTIVATE ADD BUTTON
-document.getElementById("add").onclick = function(){
-	count("add", number_value);
-}
-
-// ACTIVATE SUBTRACT BUTTON
-document.getElementById("subtract").onclick = function(){
-	count("sub", number_value);
-}
-
-// ACTIVATE MULTIPLY BUTTON
-document.getElementById("multiply").onclick = function(){
-	count("mult", number_value);
-}
-
-// ACTIVATE DIVIDE BUTTON
-document.getElementById("divide").onclick = function(){
-	count("div", number_value);
-}
-
-
-
-
-
-
-
-function count(operation){
-  coount = setInterval(function(){
-    
-    //add
-    if(operation == "add"){
-      actuator = number_value.value
-      document.getElementById("result").innerHTML = counter_num;
-			counter_num += parseInt(actuator);
-    }
-    //subtract
-    if(operation == "sub"){
-      actuator = number_value.value
-      document.getElementById("result").innerHTML = counter_num;
-			counter_num -= parseInt(actuator);
-    }
-
-    //multiply
-     if(operation == "mult"){
-      actuator = number_value.value
-      document.getElementById("result").innerHTML = counter_num;
-			counter_num *= parseInt(actuator);
-    }
-
-    //divide
-     if(operation == "div"){
-      actuator = number_value.value
-      document.getElementById("result").innerHTML = counter_num;
-			counter_num /= parseInt(actuator);
-    }
-  }, 1000);
-}
-
-document.getElementById("stopping").onclick = function(){
-  clearInterval(coount);
-}
-
-// HTML
-
-
 
 
 
