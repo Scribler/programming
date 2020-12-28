1fb63c65dd29b5043f0a333edb8c0557a9e8ff9d
Steven McIntyre <steven.mc2225@gmail.com>
2020-12-28 12:20:53 -0800

    update

--- web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
+++ web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
@@ -1,23 +1,45 @@
+
 // Long Version
+//
 // $(document).ready(function(){ // check if page is loaded
   // $("p").hide(); // hide all paragraphs on page
 // });
 
+
 // Short Version
 $(function(){ // check if page is loaded
-  // $("p").hide(); // hide all paragraphs on page
   $("*").css("font-size", "18px");
   // $("div > p").css("text-align","center"); // center paragraphs that are children of divs
-  // $("div ~ p").css("text-align","center"); // center paragraphs that are siblings of divs
+  $("div ~ p").css("text-align","center"); // center paragraphs that are siblings of divs
   $("div p").css("text-align","center"); // center paragraphs that are siblings of divs
+
+// Selecting a 'class'
   $(".yellowbox").css("background-color", "purple");
+// Selecting a 'id'
   $("#great").css("background-color", "yellow");
   $(".orange").css("color", "orange");
+
+// Selecting bluebox of div
   $("div.bluebox").css("font-family", "sans-serif");
-  
-  $("div:nth-child(2), div > div:last-child").css("color", "#74E2FE"); // effects the paragraph that is the first child of its container
-  // $("div > div:last-child").css("text-decoration", "underline"); // effects the paragraph that is the first child of its container
+
+// Selecting two separate elements to apply the same styling
+// effects the paragraph that is the first child of its container
+  $("div:nth-child(2), div > div:last-child").css("color", "#74E2FE");
+
+// Effects the paragraph that is the first child of its container
+  // $("div > div:last-child").css("text-decoration", "underline"); 
+
+// select anything with an 'href'
+  $("[href]").css("color", "red");
 });
+
+
+
+
+
+
+
+
 
 // Wrong Version (doesn't check if page is loaded)
 // $("p").hide(); // This does the same as above but isn't best practice
