3b239e13af90376c65eb41ae5ccd28208a63a606
Steven McIntyre <steven.mc2225@gmail.com>
2020-12-28 11:36:10 -0800

    update

--- web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
+++ web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
@@ -1,12 +1,23 @@
 // Long Version
-$(document).ready(function(){ // check if page is loaded
-  $("p").hide(); // hide all paragraphs on page
-});
+// $(document).ready(function(){ // check if page is loaded
+  // $("p").hide(); // hide all paragraphs on page
+// });
 
 // Short Version
 $(function(){ // check if page is loaded
-  $("p").hide(); // hide all paragraphs on page
+  // $("p").hide(); // hide all paragraphs on page
+  $("*").css("font-size", "18px");
+  // $("div > p").css("text-align","center"); // center paragraphs that are children of divs
+  // $("div ~ p").css("text-align","center"); // center paragraphs that are siblings of divs
+  $("div p").css("text-align","center"); // center paragraphs that are siblings of divs
+  $(".yellowbox").css("background-color", "purple");
+  $("#great").css("background-color", "yellow");
+  $(".orange").css("color", "orange");
+  $("div.bluebox").css("font-family", "sans-serif");
+  
+  $("div:nth-child(2), div > div:last-child").css("color", "#74E2FE"); // effects the paragraph that is the first child of its container
+  // $("div > div:last-child").css("text-decoration", "underline"); // effects the paragraph that is the first child of its container
 });
 
 // Wrong Version (doesn't check if page is loaded)
-$("p").hide(); // This does the same as above but isn't best practice
+// $("p").hide(); // This does the same as above but isn't best practice
