4fa3e5fd562f916282e501add081f0bfb3824973
Steven McIntyre <steven.mc2225@gmail.com>
2021-01-13 22:20:10 -0800

    cleaned up javascript an HTML file and got back up to speed on lesson.  Ready to continue

--- web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
+++ web-dev-course/4.JQUERY/1.general-test-page/js/scripts.js
@@ -1,51 +1,51 @@
-// import from '../../../ultils/_variables'
+// LONG VERSION - same as: "$(function(){});"
+$(document).ready(function(){ // check if page is loaded
+});
 
-console.log("Font-size is different because of javascript. It's not a css problem. ;)")
-// Long Version
-//
-// $(document).ready(function(){ // check if page is loaded
-  // $("p").hide(); // hide all paragraphs on page
-// });
+// WRONG VERSION(doesn't check if page is loaded)
+// $("p").hide(); // This does the same as above but isn't best practice
 
-
-// Short Version
+// SHORT VERSION
 $(function(){ // check if page is loaded
-  $("*").css("font-size", "16px");
-  // $("div > p").css("text-align","center"); // center paragraphs that are children of divs
-  $("div ~ p").css("text-align","center"); // center paragraphs that are siblings of divs
-  // $("div p").css("text-align","center"); // center paragraphs that are siblings of divs
 
-// Selecting a 'class'
-  $(".yellowbox").css("background-color", "$lightpurple");
-// Selecting a 'id'
-  $("#great").css("background-color", "$lightyellow");
+// MAKES ALL PARAGRAPHS DISSAPEAR WHEN BUTTON CLICKED
+  $("#disappear").click(function(){
+    $("p").hide(); // hide all paragraphs on page
+  });
+
+
+
+  $("*").css("font-size", "16px");
+  $("div > p").css("text-align","center"); // center paragraphs that are children of divs
+  $("div ~ p").css("text-align","center"); // center paragraphs that are siblings of divs
+  $("div p").css("text-align","center"); // center paragraphs that are siblings of divs
+
+  $(".yellowbox").css("background-color", "purple"); // Selecting a 'class'
+  $("#great").css("background-color", "yellow"); // Selecting a 'id'
   $(".orange").css("color", "orange");
 
-// Selecting bluebox of div
-  $("div.bluebox").css("font-family", "sans-serif");
+  $("div.bluebox").css("font-family", "sans-serif"); // Selecting bluebox of div
 
 // Selecting two separate elements to apply the same styling
 // effects the paragraph that is the first child of its container
   $("div:nth-child(2), div > div:last-child").css("color", "#74E2FE");
 
 // Effects the paragraph that is the first child of its container
-  // $("div > div:last-child").css("text-decoration", "underline"); 
+  $("div > div:last-child").css("text-decoration", "underline");
 
-// select anything with an 'href'
-  $("[href]").css("color", "red");
+  $("[href]").css("color", "red"); // select anything with an 'href'
   $("a").css({
     color: "aqua",
     backgroundColor: "black"
   });
 
-// list items
-  $("li:first-child, li:last-child").css({
+  $("li:first-child, li:last-child").css({ // list items
     backgroundColor: "grey",
     color: "yellow"
   });
 
-$("tr:even").css("background-color", "#bbf362");
-$("tr:odd").css("background-color", "#811c90");
+$("tr:even").css("background-color", "#bbf362"); // table row's even numbers set color
+$("tr:odd").css("background-color", "#811c90"); // table row's odd numbers set color
 
 $(":input").css("background-color", "#1da492");
 $(":Submit").css("background-color", "#B094FE");
@@ -55,9 +55,3 @@ $("div:has(table)").css("background-color", "#AFFFF5");
 });
 
 
-
-
-
-
-// Wrong Version (doesn't check if page is loaded)
-// $("p").hide(); // This does the same as above but isn't best practice
